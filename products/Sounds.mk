be67d1c570202687c522e9fa2df404d8e25ea198
fiqys <elsifycritic@gmail.com>
2026-08-11 11:59:43 +0300

    sounds: Setup repo
    Signed-off-by: fiqys <elsifycritic@gmail.com>

---
+++ products/Sounds.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/nothing/sounds/common/common-vendor.mk)
+
+# Defaults
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.ringtone=01_crossing.ogg \
+    ro.config.notification_sound=01_oi!.ogg \
+    ro.config.alarm_alert=01_trad.ogg
