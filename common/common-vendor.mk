307cf1f9a9bef3a90c1893dbdae050432efdc76d
fiqys <elsifycritic@gmail.com>
2026-08-11 11:52:41 +0300

    sounds: Generate from FroggerPro_B4.1-260323-1635
    Signed-off-by: fiqys <elsifycritic@gmail.com>

---
+++ common/common-vendor.mk
@@ -0,0 +1,68 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/nothing/sounds/common
+
+PRODUCT_COPY_FILES += \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/01_trad.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/01_trad.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/02_crab.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/02_crab.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/03_repeater.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/03_repeater.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/04_pace.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/04_pace.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/05_arp.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/05_arp.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/06_reflect.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/06_reflect.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/07_fidget.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/07_fidget.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/08_saw.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/08_saw.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/09_grains.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/09_grains.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/alarms/10_shake.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/alarms/10_shake.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/01_oi!.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/01_oi!.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/02_swanny.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/02_swanny.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/03_doub.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/03_doub.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/04_hum.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/04_hum.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/05_flap.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/05_flap.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/06_sharp.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/06_sharp.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/07_cuckoo.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/07_cuckoo.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/08_rude.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/08_rude.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/09_croak.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/09_croak.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/10_lonba.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/10_lonba.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/chemical_matrix.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/chemical_matrix.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/notifications/diode_matrix.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/notifications/diode_matrix.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/01_crossing.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/01_crossing.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/02_ripple.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/02_ripple.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/03_pulse.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/03_pulse.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/04_wings.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/04_wings.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/05_dolphin.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/05_dolphin.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/06_anna.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/06_anna.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/07_sticks.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/07_sticks.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/08_wizard.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/08_wizard.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/09_hammer.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/09_hammer.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/10_clwb.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/10_clwb.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/chemical_nothing.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/chemical_nothing.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ringtones/diode_nothing.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ringtones/diode_nothing.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/ChargingReverse.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/ChargingReverse.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/ChargingStarted.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/ChargingStarted.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/Dock.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/Dock.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/Effect_Tick.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/Effect_Tick.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/InCallNotification.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/InCallNotification.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/KeypressDelete.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/KeypressDelete.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/KeypressInvalid.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/KeypressInvalid.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/KeypressReturn.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/KeypressReturn.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/KeypressSpacebar.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/KeypressSpacebar.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/KeypressStandard.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/KeypressStandard.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/Lock.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/Lock.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/LowBattery.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/LowBattery.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/NFCFailure.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/NFCFailure.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/NFCInitiated.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/NFCInitiated.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/NFCSuccess.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/NFCSuccess.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/NFCTransferComplete.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/NFCTransferComplete.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/NFCTransferInitiated.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/NFCTransferInitiated.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/Trusted.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/Trusted.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/Undock.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/Undock.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/Unlock.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/Unlock.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/VideoRecord.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/VideoRecord.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/VideoStop.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/VideoStop.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/WirelessChargingStarted.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/WirelessChargingStarted.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/camera_click.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/camera_click.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/camera_focus.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/camera_focus.ogg \
+    vendor/nothing/sounds/common/proprietary/vendor/product/media/audio/ui/screenshot.ogg:$(TARGET_COPY_OUT_VENDOR)/product/media/audio/ui/screenshot.ogg
